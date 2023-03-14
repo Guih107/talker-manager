75dbe8adc96dd82944fcf2cebb6bfaef3a72972c
Guih107 <guih107@gmail.com>
2023-03-14 17:55:30 -0300

    feat: req 2

--- src/index.js
+++ src/index.js
@@ -20,6 +20,19 @@ app.get('/talker', async (req, res) => {
   }
 });
 
+app.get('/talker/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const talkers = await fileReading();
+    const talkerById = talkers.find((talker) => talker.id === parseInt(id, 10));
+    if (!talkerById) {
+      return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    }
+    return res.status(HTTP_OK_STATUS).json(talkerById);
+  } catch (err) {
+    return res.status(404).json({ message: err.message });
+  }
+});
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
